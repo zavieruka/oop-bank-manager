038d46e6327df1fcba5fbbf42738a58857107872
zavieruka <zvkmateus@gmail.com>
2025-05-11 21:16:31 -0300

    Update SavingAccount methods replacing transfer with CashOut

--- account/savingAccount.go
+++ account/savingAccount.go
@@ -13,25 +13,25 @@ type SavingAccount struct {
 	balance       float64
 }
 
+func (c *SavingAccount) CashOut(value float64) string {
+	canCashOut := value <= c.balance && value > 0
+	if value <= 0 {
+		return "cash out amount must be greater than zero"
+	}
+	if canCashOut {
+		c.balance -= value
+		return fmt.Sprintf("cash out successful! New balance: %.2f", c.balance)
+	}
+	return fmt.Sprintf("insufficient funds! Current balance: %.2f", c.balance)
+
+}
+
 func (c *SavingAccount) Deposit(value float64) string {
 	if value <= 0 {
 		return "deposit amount must be greater than zero"
 	}
 	c.balance += value
 	return fmt.Sprintf("deposit successful! New balance: %.2f", c.balance)
-}
-
-func (c *SavingAccount) Transfer(value float64, target *CheckingAccount) string {
-	canTransfer := value <= c.balance && value > 0
-	if value <= 0 {
-		return "transfer amount must be greater than zero"
-	}
-	if canTransfer {
-		c.balance -= value
-		target.balance += value
-		return fmt.Sprintf("transfer successful! New balance: %.2f", c.balance)
-	}
-	return fmt.Sprintf("insufficient funds! Current balance: %.2f", c.balance)
 }
 
 func (c *SavingAccount) GetBalance() float64 {
